e938201b8eee916c5a862938b2a36f4f6a2ebe3f
Destra <dewans047@gmail.com>
2023-11-13 02:07:51 +0700

    mengupdate js Day 4

--- Day 4/javascript/myproject.js
+++ Day 4/javascript/myproject.js
@@ -53,14 +53,17 @@ function renderBlog() {
     console.log(dataBlog[index]);
 
     document.getElementById("contents").innerHTML += `
-        <div class="blog-list-item">
+        <div id="fullTime class="blog-list-item">
                     <div class="thumb">
                         <img src="${dataBlog[index].image}" alt="Thumbnail">
                     </div>
                     <h4>
                         <a href="blog-detail.html">${dataBlog[index].project}</a>
                     </h4>
-                    <p class="duration">durasi : ${dataBlog[index].duration} bulan</p>
+                    <div  id="distanceTime">
+                    <p  class="duration">durasi : ${dataBlog[index].duration} bulan</p>
+                    </div>
+                   
                     <p>${dataBlog[index].description}</p>
                     <div class="icon-technology">
                         ${(dataBlog[index].checkbox1 ? `<img src="/Day 4/image-repository/node-js.svg" class="show" alt="Node JS">` : `<img src="/Day 4/image-repository/node-js.svg" alt="/Node Js">`)}
@@ -83,3 +86,86 @@ function monthDiff(startDate, endDate) {
   months += endDate.getMonth();
   return months <= 0 ? 0 : months;
 }
+
+
+function getFullTime(time) {
+  let monthName = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+  ];
+
+  let date = time.getDate();
+  let monthIndex = time.getMonth();
+  let year = time.getFullYear();
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+
+  if (hours <= 9) {
+      hours = "0" + hours;
+  }
+  if (minutes <= 9) {
+      minutes = "0" + minutes;
+  }
+
+  return `${date} ${monthName[monthIndex]} ${year} ${hours}:${minutes} WIB`;
+  }
+
+  function getDistanceTime(time) {
+  let timeNow = new Date();
+  let timePost = time;
+
+  let distance = timeNow - timePost;
+
+  let milisecond = 1000; // 1000 milisecond = 1 detik
+  let secondInHours = 3600; // 3600 detik = 1 jam
+  let hoursInDays = 24; // 24 jam = 1 hari
+
+  let distanceDay = Math.floor(
+      distance / (milisecond * secondInHours * hoursInDays)
+  );
+  let distanceHours = Math.floor(distance / (milisecond * 60 * 60));
+  let distanceMinutes = Math.floor(distance / (milisecond * 60));
+  let distanceSeconds = Math.floor(distance / milisecond);
+
+  if (distanceDay > 0) {
+      return `${distanceDay} day ago`;
+  } else if (distanceHours > 0) {
+      return `${distanceHours} hour ago`;
+  } else if (distanceMinutes > 0) {
+      return `${distanceMinutes} minute ago`;
+  } else {
+      return `${distanceSeconds} second ago`;
+  }
+  }
+
+  function renderBlog() {
+  // Replace with the actual timestamp of your blog post
+  let blogPostTimestamp = new Date(/* set your timestamp here */);
+
+  // Get the full time and distance time
+  let fullTimeResult = getFullTime(blogPostTimestamp);
+  let distanceTimeResult = getDistanceTime(blogPostTimestamp);
+
+  // Update the content of the HTML elements
+  document.getElementById('fullTime').innerHTML = fullTimeResult;
+  document.getElementById('distanceTime').innerHTML = distanceTimeResult;
+  }
+
+  // Call renderBlog initially
+  renderBlog();
+
+  // Set up the setInterval function to update every 3 seconds
+  setInterval(function () {
+  // Update the content by calling renderBlog
+  renderBlog();
+  }, 3000);
