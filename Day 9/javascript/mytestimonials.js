c47779159ebf69b5801dbcb5e77b06963ddb2139
Destra <dewans047@gmail.com>
2023-11-17 02:50:01 +0700

    add promises, async/await, and AJAX

--- Day 9/javascript/mytestimonials.js
+++ Day 9/javascript/mytestimonials.js
@@ -119,42 +119,176 @@ const testimonialData = [
 
 
   // To implement asynchronous programming using promises, async/await, and AJAX in your JavaScript code
+// mytestimonials.js
 
-  document.addEventListener("DOMContentLoaded", () => {
-    fetchData();
-});
+// Function to fetch testimonials from a server asynchronously
+// mytestimonials.js
 
-async function fetchData() {
-    try {
-        const response = await fetch('https://api.npoint.io/7838abb77d1f8079a915'); // Replace 'your-endpoint' with your actual endpoint
-        const data = await response.json();
-        displayData(data);
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
+// Function to fetch testimonials from the provided API
+async function fetchTestimonials() {
+  try {
+    const response = await fetch('https://api.npoint.io/7838abb77d1f8079a915');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching testimonials:', error);
+    throw error;
+  }
 }
 
-function displayData(data) {
-    const testimonialContainer = document.getElementById('testimonial__container');
+// Function to render testimonials on the page
+function renderTestimonials(testimonials) {
+  const testimonialContainer = document.getElementById('testimonial__container');
+  testimonialContainer.innerHTML = ''; // Clear existing content
 
-    // Clear existing content
-    testimonialContainer.innerHTML = '';
+  testimonials.forEach((testimonial) => {
+    const testimonialCard = document.createElement('div');
+    testimonialCard.className = 'testimonial__card';
 
-    // Assuming data is an array of objects with properties like comment, stars, author, etc.
-    data.forEach(item => {
-        const testimonialCard = document.createElement('div');
-        testimonialCard.className = 'testimonial__card';
-        testimonialCard.innerHTML = `
-            <img class="testimonial__cardImage" src="${item.image}">
-            <i class="testimonial__comment">"${item.comment}"</i>
-            <p class="testimonial__stars" id="reviewStars">${generateStarIcons(item.stars)}</p>
-            <p class="testimonial__author">- ${item.author}</p>
-        `;
-        testimonialContainer.appendChild(testimonialCard);
+    testimonialCard.innerHTML = `
+      <img class="testimonial__cardImage" src="${testimonial.image}" alt="User Image">
+      <i class="testimonial__comment">${testimonial.comment}</i>
+      <p class="testimonial__stars" id="reviewStars">
+        ${Array.from({ length: testimonial.stars }, () => '<i class="fa-solid fa-star"></i>').join('')}
+      </p>
+      <p class="testimonial__author">- ${testimonial.author}</p>
+    `;
+
+    testimonialContainer.appendChild(testimonialCard);
+  });
+}
+
+// Function to filter testimonials based on rating
+function filterTestimonial(rating) {
+  // You can implement filtering logic here if needed
+  // For simplicity, we'll refetch all testimonials and render them
+  fetchTestimonials().then(renderTestimonials);
+}
+
+// Initial load of testimonials when the page loads
+document.addEventListener('DOMContentLoaded', () => {
+  fetchTestimonials().then(renderTestimonials);
+});
+
+
+
+function fetchTestimonials() {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '/api/testimonials', true);
+
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        const testimonials = JSON.parse(xhr.responseText);
+        resolve(testimonials);
+      } else {
+        reject(new Error(`Failed to fetch testimonials. Status: ${xhr.status}`));
+      }
+    };
+
+    xhr.onerror = function () {
+      reject(new Error('Network error while fetching testimonials.'));
+    };
+
+    xhr.send();
+  });
+}
+
+// Function to render testimonials on the page
+function renderTestimonials(testimonials) {
+  const testimonialContainer = document.getElementById('testimonial__container');
+  testimonialContainer.innerHTML = '';
+
+  testimonials.forEach(testimonial => {
+    const card = document.createElement('div');
+    card.className = 'testimonial__card';
+
+    // Create and append other elements like image, comment, stars, and author to the card
+
+    testimonialContainer.appendChild(card);
+  });
+}
+
+// Function to filter testimonials based on rating
+function filterTestimonial(rating) {
+  // Call the fetchTestimonials function to get testimonials
+  fetchTestimonials()
+    .then(testimonials => {
+      // Filter testimonials based on rating
+      const filteredTestimonials = testimonials.filter(testimonial => {
+        return testimonial.rating === rating;
+      });
+
+      // Render the filtered testimonials on the page
+      renderTestimonials(filteredTestimonials);
+    })
+    .catch(error => {
+      console.error(error);
     });
 }
 
-function generateStarIcons(stars) {
-    const starIcons = Array.from({ length: stars }, () => '<i class="fa-solid fa-star"></i>');
-    return starIcons.join('');
+// Initial load: Fetch all testimonials and render on the page
+document.addEventListener('DOMContentLoaded', function () {
+  fetchTestimonials()
+    .then(testimonials => {
+      renderTestimonials(testimonials);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
+
+// Asynchronous function using Promises
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // Simulate fetching data from an API
+      const data = { message: 'Data fetched successfully!' };
+      resolve(data);
+      // Uncomment the line below to simulate an error
+      // reject(new Error('Error fetching data!'));
+    }, 2000);
+  });
 }
+
+// Using the asynchronous function with Promises
+fetchData()
+  .then((data) => {
+    console.log(data.message);
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
+
+
+  // Asynchronous function using AJAX
+function fetchDataWithAjax() {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.npoint.io/7838abb77d1f8079a915', true);
+
+    xhr.onload = function () {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        const responseData = JSON.parse(xhr.responseText);
+        resolve(responseData);
+      } else {
+        reject(new Error(`Error fetching data. Status: ${xhr.status}`));
+      }
+    };
+
+    xhr.onerror = function () {
+      reject(new Error('Network error'));
+    };
+
+    xhr.send();
+  });
+}
+
+// Using the asynchronous function with AJAX
+fetchDataWithAjax()
+  .then((data) => {
+    console.log('Data:', data);
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
